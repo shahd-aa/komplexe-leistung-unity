6b3423736622da4aa4e096323d24c82b631ad677
shahd <99558154+shahd-aa@users.noreply.github.com>
2025-10-11 14:30:39 +0200

    Update SubtitleScript.cs

--- SubtitleScript.cs
+++ SubtitleScript.cs
@@ -1,37 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-using UnityEngine.Events;
-using UnityEngine.Playables;
+using System.Collections.Generic;
 
-public class SubtitleScript: MonoBehaviour
+public class SubtitleScript : MonoBehaviour
 {
-    // Reference to PlayableDirector
-    public PlayableDirector playableDirector;
-    
-    // Reference to subtitle text
     public TMP_Text subtitleText;
-    
-    // Reference to subtitle panel
-    public GameObject subtitlePanel;
-    
-    // Reference to the actual subtitle text
     public List<string> subtitles = new List<string>();
     
-    void Start()
-    {
-        // Shows subtitle panel
-        subtitlePanel.SetActive(true);
-        
-        // Starts timeline playing
-        playableDirector.Play();
-        
-        // Adds listener for when timeline ends
-        playableDirector.stopped += OnTimelineEnded;
-    }
-    
+    // Just displays subtitle - doesn't control timeline!
     public void ShowSubtitle(int index) 
     {
         if (index >= 0 && index < subtitles.Count)
@@ -40,33 +16,9 @@ public class SubtitleScript: MonoBehaviour
         }
     }
     
-    public void ShowSubtitle0()
-    {
-        ShowSubtitle(0);
-    }
-    
-    public void ShowSubtitle1()
-    {
-        ShowSubtitle(1);
-    }
-    
-    public void ShowSubtitle2()
-    {
-        ShowSubtitle(2);
-    }
-    
-    public void ShowSubtitle3()
-    {
-        ShowSubtitle(3);
-    }
-    
-    private void OnTimelineEnded(PlayableDirector director)
-    {
-        Debug.Log("Timeline ended!");
-    }
-    
-    void Update()
-    {
-        
-    }
+    // Wrapper methods for Timeline Signals
+    public void ShowSubtitle0() { ShowSubtitle(0); }
+    public void ShowSubtitle1() { ShowSubtitle(1); }
+    public void ShowSubtitle2() { ShowSubtitle(2); }
+    public void ShowSubtitle3() { ShowSubtitle(3); }
 }
