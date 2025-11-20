d92544b71f890649cc7ee8b369be18d248cc5fcc
shahd <99558154+shahd-aa@users.noreply.github.com>
2025-11-20 20:26:35 +0100

    Enhance quiz UI with retry functionality
    Added retry button and improved UI interactions.

--- StateManager2.cs
+++ StateManager2.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using TMPro;
 using DG.Tweening;
 using Unity.Cinemachine;
+using UnityEngine.SceneManagement;
 
 public class StateManager2 : MonoBehaviour
 {
@@ -22,12 +23,14 @@ public class StateManager2 : MonoBehaviour
     // Quiz elements
     public GameObject questionPanel;
     public GameObject answerOptionsPanel;
-    public Button submitButton;
     public GameObject characterPanel;
     public Animator characterAnimator;
     public TMP_Text feedbackText;
     public TMP_Text explanationText;
+
     public Button nextButton;
+    public Button submitButton;
+    public Button retryButton;
 
     // Question data
     public QuestionData currentQuestion;
@@ -40,6 +43,8 @@ public class StateManager2 : MonoBehaviour
     public GameObject lettersPanel;
     public PlayableDirector timeline;
     public GameObject blurImage;
+    public GameObject confettiRight;
+    public GameObject confettiLeft;
 
     // Cinemachine
     public CinemachineCamera followCamera;
@@ -70,8 +75,6 @@ public class StateManager2 : MonoBehaviour
         frontCamera.gameObject.SetActive(false);
         mainCameraBrain.enabled = false;
 
-        timeline.gameObject.SetActive(false);
-
         // Get all answer options
         foreach (Transform child in answerOptionsPanel.transform)
         {
@@ -92,6 +95,7 @@ public class StateManager2 : MonoBehaviour
         }
 
         // Setup
+        DisableBackgroundRaycasts();
         HideAllPanels();
         StartCoroutine(TitleScreenSequence());
         nextButton.onClick.AddListener(OnNextQuestion);
@@ -104,12 +108,14 @@ public class StateManager2 : MonoBehaviour
         quizPanel.SetActive(false);
         feedbackPanel.SetActive(false);
         characterPanel.SetActive(false);
+        confettiRight.SetActive(false);
+        confettiLeft.SetActive(false);
     }
 
     IEnumerator TitleScreenSequence()
     {
         timeline.time = 0;
-        timeline.Stop();
+        timeline.Pause();
 
         titlePanel.SetActive(true);
         yield return new WaitForSeconds(titleDisplayTime);
@@ -120,10 +126,6 @@ public class StateManager2 : MonoBehaviour
     void StartCutscene()
     {
         subtitlePanel.SetActive(true);
-
-        // Re-enable timeline
-        timeline.gameObject.SetActive(true);
-
         followCamera.gameObject.SetActive(true);
         frontCamera.gameObject.SetActive(true);
         mainCameraBrain.enabled = true;
@@ -136,12 +138,19 @@ public class StateManager2 : MonoBehaviour
     void OnCutsceneEnded(PlayableDirector pd)
     {
         Debug.Log("CUTSCENE ENDED!");
-        pd.Stop();
+
+        //Hold at the last frame
+        pd.time = 0;
+        pd.Pause();
+
         StartCoroutine(ShowQuizSequence());
     }
 
     IEnumerator ShowQuizSequence()
     {
+        // Delay before popping up the UI 
+        yield return new WaitForSeconds(2f);
+
         subtitlePanel.SetActive(false);
 
         // Show quiz panel
@@ -151,7 +160,10 @@ public class StateManager2 : MonoBehaviour
         questionPanel.SetActive(false);
         answerOptionsPanel.SetActive(false);
         submitButton.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
         blurImage.SetActive(false);
+        confettiRight.SetActive(false);
+        confettiLeft.SetActive(false);
         characterHappy.SetActive(false);
         characterUpset.SetActive(false);
 
@@ -162,9 +174,20 @@ public class StateManager2 : MonoBehaviour
         LoadQuestionData();
 
         // Show quiz elements
+        RemoveCameraNoise();
         questionPanel.SetActive(true);
         answerOptionsPanel.SetActive(true);
         blurImage.SetActive(true);
+
+        // FIX: Disable raycast on blur image so it doesn't block buttons
+        Image blurImg = blurImage.GetComponent<Image>();
+        if (blurImg != null)
+        {
+            blurImg.raycastTarget = false;
+        }
+
+        // Move blur to back
+        blurImage.transform.SetAsFirstSibling();
 
         // Hide all options first
         foreach (GameObject option in answerOptions)
@@ -173,12 +196,30 @@ public class StateManager2 : MonoBehaviour
         }
 
         // Show options one by one
-        foreach (GameObject option in answerOptions)
+        Transform panelTransform = answerOptionsPanel.transform;
+
+        for (int i = 0; i < panelTransform.childCount; i++)
         {
-            option.SetActive(true);
-            PopOut(option.GetComponent<RectTransform>());
+            Transform answerTransform = panelTransform.GetChild(i);
+            GameObject answerOption = answerTransform.gameObject;
+
+            answerOption.SetActive(true);
+
+            // Make button interactable
+            Button btn = answerOption.GetComponent<Button>();
+            if (btn != null)
+            {
+                btn.interactable = true;
+            }
+
+            // Animate the answer option
+            PopOut(answerOption.GetComponent<RectTransform>());
+
             yield return new WaitForSeconds(delayBetweenAnswerOptions);
         }
+
+        // Bring panel to front
+        answerOptionsPanel.transform.SetAsLastSibling();
 
         // Show submit button
         submitButton.gameObject.SetActive(true);
@@ -235,53 +276,77 @@ public class StateManager2 : MonoBehaviour
             explanationText.text = currentQuestion.explanation;
             characterPanel.SetActive(true);
             characterHappy.SetActive(true);
+            confettiRight.SetActive(true);
+            confettiLeft.SetActive(true);
             nextButton.gameObject.SetActive(true);
             Debug.Log("CORRECT! " + currentQuestion.correctFeedback);
         }
         else
         {
+            nextButton.gameObject.SetActive(false);
             feedbackText.text = currentQuestion.wrongFeedback;
-            explanationText.text = $"Correct Answer: {currentQuestion.correctAnswerIndex} {currentQuestion.explanation}";
+            explanationText.text = $"Correct Answer: {currentQuestion.correctAnswerIndex + 1}. {currentQuestion.explanation}";
             characterPanel.SetActive(true);
             characterUpset.SetActive(true);
-            submitButton.gameObject.SetActive(true);
-            if (submitText) submitText.text = "Retry";
-            submitButton.onClick.RemoveAllListeners();
-            submitButton.onClick.AddListener(OnRetry);
+
+            submitButton.gameObject.SetActive(false);
+            retryButton.gameObject.SetActive(true);
+
+            retryButton.onClick.AddListener(OnRetry);
+
             Debug.Log("WRONG! " + currentQuestion.wrongFeedback);
         }
     }
 
     void OnRetry()
     {
-        TMP_Text submitText = submitButton.GetComponentInChildren<TMP_Text>();
+        Debug.Log("OnRetry called!");
+
+        // Hide feedback panels
         feedbackPanel.SetActive(false);
-        if (submitText) submitText.text = "Submit";
+        characterPanel.SetActive(false);
+        characterHappy.SetActive(false);
+        characterUpset.SetActive(false);
+
+        // Hide retry button
+        retryButton.gameObject.SetActive(false);
+        retryButton.onClick.RemoveAllListeners();
+
+        // Show quiz elements
+        questionPanel.SetActive(true);
+        answerOptionsPanel.SetActive(true);
+
+        // Shuffle answers
+        Shuffle();
+
+        // Reset answer system
+        answerSystem.ResetButtons();
+
+        // Reload question with shuffled answers
+        LoadQuestionData();
+
+        // Show submit button (not retry)
+        submitButton.gameObject.SetActive(true);
         submitButton.onClick.RemoveAllListeners();
         submitButton.onClick.AddListener(answerSystem.CheckAnswer);
-        submitButton.gameObject.SetActive(true);
-        Shuffle();
-        answerSystem.ResetButtons();
     }
 
     void Shuffle()
     {
-        for (int i = currentQuestion.answers.Count - 1; i > 1; i--)
+        // store the correct answer value before shuffling
+        string correctAnswer = currentQuestion.answers[currentQuestion.correctAnswerIndex];
+
+        int n = currentQuestion.answers.Count;
+        for (int i = n - 1; i > 0; i--)
         {
-            randomNumber = Random.Range(0, i + 1);
-
-            if (currentQuestion.correctAnswerIndex == i)
-            {
-                currentQuestion.correctAnswerIndex = randomNumber;
-            }
-            else if (currentQuestion.correctAnswerIndex == randomNumber)
-            {
-                currentQuestion.correctAnswerIndex = i;
-            }
-
-            (currentQuestion.answers[i], currentQuestion.answers[randomNumber]) =
-                (currentQuestion.answers[randomNumber], currentQuestion.answers[i]);
+            int j = Random.Range(0, i + 1);
+            // swap answers[i] and answers[j]
+            (currentQuestion.answers[i], currentQuestion.answers[j]) =
+                (currentQuestion.answers[j], currentQuestion.answers[i]);
         }
+
+        // update correctAnswerIndex after shuffle
+        currentQuestion.correctAnswerIndex = currentQuestion.answers.IndexOf(correctAnswer);
     }
 
     void OnNextQuestion()
@@ -297,6 +362,43 @@ public class StateManager2 : MonoBehaviour
         if (timeline != null)
         {
             timeline.stopped -= OnCutsceneEnded;
+        }
+    }
+
+    void RemoveCameraNoise()
+    {
+        var perlin = followCamera.GetCinemachineComponent(CinemachineCore.Stage.Noise) as CinemachineBasicMultiChannelPerlin;
+        if (perlin != null)
+        {
+            perlin.enabled = false;
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    void DisableBackgroundRaycasts()
+    {
+        // Disable raycast on background panels
+        if (quizPanel != null)
+        {
+            Image panelImg = quizPanel.GetComponent<Image>();
+            if (panelImg != null) panelImg.raycastTarget = false;
+        }
+
+        if (feedbackPanel != null)
+        {
+            Image panelImg = feedbackPanel.GetComponent<Image>();
+            if (panelImg != null) panelImg.raycastTarget = false;
+        }
+
+        if (questionPanel != null)
+        {
+            Image panelImg = questionPanel.GetComponent<Image>();
+            if (panelImg != null) panelImg.raycastTarget = false;
         }
     }
 }
